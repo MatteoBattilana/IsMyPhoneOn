62fbeecfdef2a6caae8a2bdad4a2df92ae73e414
Matteo Battilana <matbat96@gmail.com>
2015-11-25 20:40:18 +0100

    Reduced size from 1.8MB to 238KB

--- app/src/main/java/ismyphoneon/battilanamatteo/com/ismyphoneon/MainActivity.java
+++ app/src/main/java/ismyphoneon/battilanamatteo/com/ismyphoneon/MainActivity.java
@@ -5,20 +5,12 @@ import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 public class MainActivity extends Activity {
     TextView txt_yes,txt_req;
